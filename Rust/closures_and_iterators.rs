e655ef00d5b89cee4ab75e8f95d4a9f91b4fef1d
Ronald Huidrom <ronhuidrom@gmail.com>
2025-03-25 17:55:47 +0530

    Modified Rust/closures_and_iterators.rs

--- Rust/closures_and_iterators.rs
+++ Rust/closures_and_iterators.rs
@@ -69,5 +69,65 @@ a = add_x(6);
 let add_x = move |x| x + a; // capture a, by value
 a = add_x(6);
 
+// =============================================================
+// Iterator ====================================================
+// =============================================================
 
+/*
+ * iter(): burrows elements immutably (&T),
+ *         use when you only need to scan/read data,
+ *         original collection remains usable after iteration.
+ *
+ * iter_mut(): burrows elements mutably (&mut T)
+ *             use when you need to modify elements in place,
+ *             original collection remains usable but cannot be accessed during iteration.
+ *
+ * into_iter(): takes ownership of elements (T),
+ *              use when you want to consume/transform the collection,
+ *              original collection is no longer usable after iteration.
+ */
+
+let v = vec![1, 2, 3, 4, 5];
+let vect: Vec<_> = v.iter().         // immutable borrow (&T)
+    map(|x| x * x).                  // applies to each element in the iterator, returns an iterator
+    filter(|&x| x > 5).              // pick only those elements returning true
+    collect();                       // materialize into a vector, needs annotation such as Vec<_> or
+                                     //                            .collect::<Vec<_>>()
+
+let vect: Vec<_> = v.into_ter().     // v can be used again because it was not consumed
+    map(|x| x * x).
+    filter(|x| x > 5).
+    collect();
+
+// println!("{:?}", v); // v was already consumed
+
+let v = vec![String::from("Vito"), String::from("Michael")];
+
+let vect: Vec<_> = v.into_iter().    // consume v
+    map(|x| x + " Huidrom").                  
+    filter(|x| x.len() > 12).
+    collect();
+
+let v = vec![true, true, false, true, false, false, false, true];
+
+let vect: Vec<_> = v.iter().         // immutable burrow (&T)
+    map(|x| !x).                     // true -> false and false -> true
+    filter(|&x| x).
+    collect();
+
+// .iter()             useful for read-only pipelines with map(), filter() and fold()
+// .iter_mut()         rarely used with map(), filter() and fold()
+// .into_iter()        powerful for transformations that consume data
+
+// fold() reduces a collection to a single value by repeatedly applying an operation.
+
+fn fold<B, F>(self, init: B, f: F)
+where
+    F: FnMut(B, Self::Item) -> B,
+
+// init       initial accumulator value, like 0 for sums, Vec::new() for collecting
+// f          closure that takes the accumulator (acc) and the current item,
+//                                                     and returns a new accumulator
+
+// Examples of fold()
 
