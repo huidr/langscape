6a3191861656196d9a1d6248055dca65c5445b3a
Ronald Huidrom <ronhuidrom@gmail.com>
2025-06-07 00:24:52 +0530

    Modify Lean/io.lean

--- Lean/io.lean
+++ Lean/io.lean
@@ -1,4 +1,5 @@
 -- IO actions
+-- Basics of propositions and proofs
 
 def helloWorld : IO Unit := IO.println "Hello, World"
 
@@ -31,7 +32,8 @@ def nTimes (action : IO Unit) : Nat → IO Unit
     action
     nTimes action k
 
-def nTimesInputGreeter (k : Nat) : IO Unit := nTimes receiveInputGreeter k
+def nTimesInputGreeter (k : Nat) : IO Unit :=
+  nTimes receiveInputGreeter k
 
 -- A list of IO actions
 def listActions : IO Unit → Nat → List (IO Unit)
@@ -46,14 +48,72 @@ def runActionsList : List (IO Unit) → IO Unit
     runActionsList xs
 
 def someListofActions := listActions receiveInputGreeter 3
-def anotherListofActions := receiveInputGreeter :: timesGreeter 2 "Lean" :: helloWorld :: timesGreeter 3 "Theorem Prover" :: []
+def anotherListofActions := 
+  receiveInputGreeter ::
+  timesGreeter 2 "Lean" ::
+  helloWorld ::
+  timesGreeter 3 "Theorem Prover" ::
+  []
 
 -- Use of partial keyword for non-terminating recursion
 -- Use of (← ) 
 partial def inputLoop : IO Unit := do
   IO.println "What's your name?"
-  let name ← IO.getLine
+  let name ← (← IO.getStdin).getLine 
   IO.println s!"Hello, {name}"
   inputLoop
 
-def main : IO Unit := inputLoop
+-- This function has problems
+partial def inputLoopUntilEmpty : IO Unit := do
+  (← IO.getStdout).putStrLn "What's your name?"
+  let name ← (← IO.getStdin).getLine
+  if name.trim == "Jerk" then
+    pure ()
+  else
+    inputLoopUntilEmpty
+
+/-
+-- Partial functions are not evaluated during type checking, because an infinite loop
+-- in a function could cause the type checker to enter an infinite loop.
+-- Furthermore, mathematical proofs are unable to inspect the definitions of partial functions,
+-- which means that programs that use them are much less amenable to formal proof. 
+-/
+
+-- def main : IO Unit := inputLoopUntilEmpty
+
+-- Propositions and proofs
+
+-- Prop is a type for propositions
+-- Further propositions are types
+def OnePlusOneIsTwo : Prop := 1 + 1 = 2
+theorem onePlusOneIsTwo : OnePlusOneIsTwo := rfl -- rfl refers to reflexity
+
+-- the above is logically equivalent to
+theorem opoit : 1 + 1 = 2 := rfl -- because propositions are types
+
+-- use the former to name propositions and refer to the names later multiple times
+-- use the latter for short/one-off proofs 
+
+-- Tactics
+
+/-
+-- Tactics are small programs that construct evidence for a proposition.
+-- These programs run in a proof state that tracks the statement that is to be proved (called the goal)
+-- along with the assumptions that are available to prove it.
+-- Running a tactic on a goal results in a new proof state that contains new goals.
+-- The proof is complete when all goals have been proven.
+-/
+
+-- To write a proof with tactics, begin the definition with by
+theorem newtheo : 1 + 1 = 2 := by -- by 
+  decide 
+
+-- The decide tactic in Lean tries to automatically decide propositions that are decidable — that is,
+-- where Lean can compute a boolean result and prove the truth or falsehood mechanically
+
+-- Connectives: ∧ ∨
+
+theorem r1 : 1 + 2 = 3 ∧ "Lean".append "Prover" = "LeanProver" := by
+  decide
+
+
